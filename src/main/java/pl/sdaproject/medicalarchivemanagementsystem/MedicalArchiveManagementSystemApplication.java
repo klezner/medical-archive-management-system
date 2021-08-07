aa348af335b9046d70f2ee3c609b74d631c6cd4d
klezner <59789908+klezner@users.noreply.github.com>
2021-08-07 10:32:56 +0200

    Initial commit

---
+++ src/main/java/pl/sdaproject/medicalarchivemanagementsystem/MedicalArchiveManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package pl.sdaproject.medicalarchivemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalArchiveManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalArchiveManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sdaproject/medicalarchivemanagementsystem/MedicalArchiveManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sdaproject.medicalarchivemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalArchiveManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
