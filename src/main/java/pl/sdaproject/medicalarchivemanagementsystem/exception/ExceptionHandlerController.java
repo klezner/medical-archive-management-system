bf046c58cc2235a5e793077160d03bced7702247
Katarzyna Brewinska <brewinska.katarzyna@gmail.com>
2021-08-08 12:52:44 +0200

    exception package refactor

--- src/main/java/pl/sdaproject/medicalarchivemanagementsystem/exception/ExceptionHandlerController.java
+++ src/main/java/pl/sdaproject/medicalarchivemanagementsystem/exception/ExceptionHandlerController.java
@@ -1,4 +1,4 @@
-package pl.sdaproject.medicalarchivemanagementsystem.exeption;
+package pl.sdaproject.medicalarchivemanagementsystem.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
