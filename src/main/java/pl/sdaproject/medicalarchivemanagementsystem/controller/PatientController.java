952aa1f63020c0e5f507fb314b7d1658b52da054
klezner <krzys3ek@gmail.com>
2021-08-21 12:05:08 +0200

    Fix code in accordance with code review

--- src/main/java/pl/sdaproject/medicalarchivemanagementsystem/controller/PatientController.java
+++ src/main/java/pl/sdaproject/medicalarchivemanagementsystem/controller/PatientController.java
@@ -20,7 +20,6 @@ import java.util.stream.Collectors;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(path = "/patient")
-@CrossOrigin(origins = "http://localhost:4200")
 public class PatientController {
 
     private final AddressService addressService;
