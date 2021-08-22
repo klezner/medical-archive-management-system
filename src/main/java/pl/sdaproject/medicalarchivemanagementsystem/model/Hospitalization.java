0bb3f9059e42e6e96997063406836e8fd69b5c85
MichalKulygin <michal.kulygin@gmail.com>
2021-08-22 10:17:52 +0200

    fixed: Hospitalization id in db (null problem)

--- src/main/java/pl/sdaproject/medicalarchivemanagementsystem/model/Hospitalization.java
+++ src/main/java/pl/sdaproject/medicalarchivemanagementsystem/model/Hospitalization.java
@@ -14,7 +14,7 @@ import java.time.LocalDate;
 public class Hospitalization {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private LocalDate hospitalizationFrom;
     private LocalDate hospitalizationTo;
